42bf2af85afea9827119b2a92acc8d22db663a92
K_on_Mac <gemigpost@hotmail.com>
2023-07-13 15:26:53 +0200

    textures

--- fragmentshader.fs
+++ fragmentshader.fs
@@ -1,6 +1,18 @@
 #version 330 core
 out vec4 FragColor;
 
-void main(){
-  FragColor = vec4(1.0f, 0.5f, 1.0f, 1.0f);
+in vec3 ourColor;
+in vec2 TexCoord;
+
+uniform sampler2D ourTexture;
+
+uniform sampler2D texture1;
+uniform sampler2D texture2;
+
+
+void main()
+{
+   FragColor = mix(texture(texture1, TexCoord), texture(texture2, TexCoord), 0.2);
+   // FragColor = texture(ourTexture, TexCoord) * vec4(ourColor, 1.0);
+    //FragColor = texture(ourTexture, TexCoord);
 }
