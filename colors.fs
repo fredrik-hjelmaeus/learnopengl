0ddc2df6234b339e5d20e95cc93c6751d9a66fc7
K_on_Mac <gemigpost@hotmail.com>
2023-07-23 12:38:56 +0200

    spotlight implemented

--- colors.fs
+++ colors.fs
@@ -9,6 +9,9 @@ struct Material {
 
 struct Light {
     vec3 position;
+    vec3 direction;
+    float cutOff;
+    float outerCutOff;
 
     vec3 ambient;
     vec3 diffuse;
@@ -29,13 +32,9 @@ uniform Light light;
 
 void main()
 {
-    // attenuation
-    float distance    = length(light.position - FragPos);
-    float attenuation = 1.0 / (light.constant + light.linear * distance + light.quadratic * (distance * distance));
-
-    // ambient
+        // ambient
     vec3 ambient = light.ambient * texture(material.diffuse, TexCoords).rgb;
-  	
+    
     // diffuse 
     vec3 norm = normalize(Normal);
     vec3 lightDir = normalize(light.position - FragPos);
@@ -47,10 +46,20 @@ void main()
     vec3 reflectDir = reflect(-lightDir, norm);  
     float spec = pow(max(dot(viewDir, reflectDir), 0.0), material.shininess);
     vec3 specular = light.specular * spec * texture(material.specular, TexCoords).rgb;  
-
+    
+    // spotlight (soft edges)
+    float theta = dot(lightDir, normalize(-light.direction)); 
+    float epsilon = (light.cutOff - light.outerCutOff);
+    float intensity = clamp((theta - light.outerCutOff) / epsilon, 0.0, 1.0);
+    diffuse  *= intensity;
+    specular *= intensity;
+    
+    // attenuation
+    float distance    = length(light.position - FragPos);
+    float attenuation = 1.0 / (light.constant + light.linear * distance + light.quadratic * (distance * distance));    
     ambient  *= attenuation; 
-    diffuse  *= attenuation;
-    specular *= attenuation;  
+    diffuse   *= attenuation;
+    specular *= attenuation;   
         
     vec3 result = ambient + diffuse + specular;
     FragColor = vec4(result, 1.0);
--- main.cpp
+++ main.cpp
@@ -197,7 +197,7 @@ int main()
 
         // be sure to activate shader when setting uniforms/drawing objects
         lightingShader.use();
-        lightingShader.setVec3("light.position", lightPos);
+        ///lightingShader.setVec3("light.position", lightPos);
         lightingShader.setVec3("viewPos", camera.Position);
         //lightingShader.setVec3("light.direction", -0.2f, -1.0f, -0.3f); 
 
@@ -208,6 +208,11 @@ int main()
         lightingShader.setFloat("light.constant",  1.0f);
         lightingShader.setFloat("light.linear",    0.09f);
         lightingShader.setFloat("light.quadratic", 0.032f);	
+        lightingShader.setVec3("light.position",  camera.Position);
+        lightingShader.setVec3("light.direction", camera.Front);
+        lightingShader.setFloat("light.cutOff",   glm::cos(glm::radians(12.5f)));
+        lightingShader.setFloat("light.outerCutOff", glm::cos(glm::radians(17.5f)));
+
 
         // material properties
         lightingShader.setFloat("material.shininess", 32.0f);
@@ -250,7 +255,7 @@ int main()
 
 
         // also draw the lamp object
-         lightCubeShader.use();
+        lightCubeShader.use();
         lightCubeShader.setMat4("projection", projection);
         lightCubeShader.setMat4("view", view);
         model = glm::mat4(1.0f);
